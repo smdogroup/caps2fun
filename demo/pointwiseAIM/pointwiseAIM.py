4e1d5c6eb7cf84fdb91a89c9167e6071981cd756
Engelstad <sengelstad6@ae-smdo-405112.ae.gatech.edu>
2022-04-08 10:42:41 -0400

    working on pointwise demo

--- demo/pointwiseAIM/pointwiseAIM.py
+++ demo/pointwiseAIM/pointwiseAIM.py
@@ -12,7 +12,7 @@ def run_pointwise(pointwise):
     os.chdir(pointwise.analysisDir)
 
     CAPS_GLYPH = os.environ["CAPS_GLYPH"]
-    for i in range(60):
+    for i in range(1):
         os.system("pointwise -b " + CAPS_GLYPH + "/GeomToMesh.glf caps.egads capsUserDefaults.glf")
 
         if os.path.isfile('caps.GeomToMesh.gma') and os.path.isfile('caps.GeomToMesh.ugrid'): break
@@ -42,8 +42,8 @@ pointwise.input.Connector_Source_Spacing = True
 
 #Domain level
 pointwise.input.Domain_Algorithm = "AdvancingFront"
-pointwise.input.Domain_Max_Layers = 15
-pointwise.input.Domain_TRex_ARLimit = 40.0
+pointwise.input.Domain_Max_Layers = 30
+pointwise.input.Domain_TRex_ARLimit = 50.0
 pointwise.input.Domain_Decay = 0.8
 
 #Block level
